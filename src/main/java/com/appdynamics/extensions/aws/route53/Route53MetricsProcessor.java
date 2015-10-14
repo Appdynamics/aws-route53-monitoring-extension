e4ecbaab206336f65c208f1779dbe0061dfdb393
satish-m <jagwar321>
2015-10-14 11:12:00 +0530

    Initial code check-in

---
+++ src/main/java/com/appdynamics/extensions/aws/route53/Route53MetricsProcessor.java
@@ -0,0 +1,59 @@
+package com.appdynamics.extensions.aws.route53;
+
+import com.amazonaws.services.cloudwatch.AmazonCloudWatch;
+import com.amazonaws.services.cloudwatch.model.Metric;
+import com.appdynamics.extensions.aws.config.MetricType;
+import com.appdynamics.extensions.aws.metric.NamespaceMetricStatistics;
+import com.appdynamics.extensions.aws.metric.StatisticType;
+import com.appdynamics.extensions.aws.metric.processors.MetricsProcessor;
+import com.appdynamics.extensions.aws.metric.processors.MetricsProcessorHelper;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+/**
+ * @author Satish Muddam
+ */
+public class Route53MetricsProcessor implements MetricsProcessor {
+
+    private static final String NAMESPACE = "AWS/Route53";
+
+    private static final String[] DIMENSIONS = {"HealthCheckId"};
+
+    private List<MetricType> metricTypes;
+
+    private Pattern excludeMetricsPattern;
+
+    public Route53MetricsProcessor(List<MetricType> metricTypes,
+                                   Set<String> excludeMetrics) {
+        this.metricTypes = metricTypes;
+        this.excludeMetricsPattern = MetricsProcessorHelper.createPattern(excludeMetrics);
+    }
+
+    public List<Metric> getMetrics(AmazonCloudWatch awsCloudWatch) {
+        return MetricsProcessorHelper.getFilteredMetrics(awsCloudWatch,
+                NAMESPACE,
+                excludeMetricsPattern,
+                DIMENSIONS);
+    }
+
+    public StatisticType getStatisticType(Metric metric) {
+        return MetricsProcessorHelper.getStatisticType(metric, metricTypes);
+    }
+
+    public Map<String, Double> createMetricStatsMapForUpload(NamespaceMetricStatistics namespaceMetricStats) {
+        Map<String, String> dimensionToMetricPathNameDictionary = new HashMap<String, String>();
+        dimensionToMetricPathNameDictionary.put(DIMENSIONS[0], "HealthCheck Id");
+
+        return MetricsProcessorHelper.createMetricStatsMapForUpload(namespaceMetricStats,
+                dimensionToMetricPathNameDictionary, false);
+    }
+
+    public String getNamespace() {
+        return NAMESPACE;
+    }
+
+}
